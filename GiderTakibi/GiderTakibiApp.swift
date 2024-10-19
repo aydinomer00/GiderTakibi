c040f2076aae165fc2ed03b422e5d033feaf6ad5
omermurataydin <omermurataydin@odakgis.com.tr>
2024-10-19 22:48:23 +0300

    Initial Commit

---
+++ GiderTakibi/GiderTakibiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiderTakibiApp.swift
+//  GiderTakibi
+//
+//  Created by Omer Murat Aydin on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GiderTakibiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
